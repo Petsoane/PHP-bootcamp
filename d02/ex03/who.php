1cfd919b19ed124c411ff1175ae3c0c246d60486
Lebogang PETSOANE <lpetsoan@c4r17s8.wethinkcode.co.za>
2019-10-11 15:14:54 +0200

    half-way through

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -1,6 +1,21 @@
 #!/usr/bin/php
 
-<?php
-$output = shell_exec("who");
-echo $output;
+<?php 
+    # create a new string.
+    $file_name = str_replace("^", date("Y-m-d"), "c4r17s8.^.logins.log");
+    echo $file_name."\n";
+    # open the login file.
+    $file = fopen("/var/log/login/$file_name", "r");
+    if ($file){
+        echo "file opend \n";
+        while (($line = fgets($file)) !== false)
+        {
+            if (preg_match('/(.*?)still logged in/', $line, $user) == 1)
+            {
+                $info = preg_replace('/[ ]{2,}|[\t]/', ' ', $user[1]);
+                echo $info;
+            }
+        }
+    }
+    fclose($file);
 ?>
